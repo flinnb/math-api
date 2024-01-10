163615b64fe70838113b02eac25e03077609f571
Barry Flinn <barry@flinn.me>
2024-01-10 12:40:59 -0500

    Quick implementation of math functionality
    Adds:
    * implementations for methods in math lib
    * handler calls to the correct methods

--- src/handler/math.js
+++ src/handler/math.js
@@ -1,11 +1,40 @@
+import * as mathlib from '../lib/math';
 /**
  * Math is an API endpoint that allows you to return a result based off inputs.
  * This MUST support all available math functions defined in the "/lib/math.js".
  *
  * @param body
+ * This assumes the body is the format:
+ * {
+ *   "operation": "{operation},
+ *   "a": {number, array of numbers},
+ *   "b": {number}
+ * }
+ * operation: the math operation you wish to perform
+ * a: the first item of the math expression, or, an array of numbers for the mean
+ * b: the second item of the math expression
  */
 export function math (body) {
+  let mathResult = null;
+
+  switch (body.operation) {
+    case "sum":
+      mathResult = mathlib.sum(body.a, body.b);
+      break;
+    case "subtract":
+      mathResult = mathlib.subtract(body.a, body.b);
+      break;
+    case "divide":
+      mathResult = mathlib.divide(body.a, body.b);
+      break;
+    case "multiply":
+      mathResult = mathlib.multiply(body.a, body.b);
+      break;
+    case "mean":
+      mathResult = mathlib.mean(body.a);
+      break;
+  }
   return {
-    result: null
+    result: mathResult
   }
 }
--- src/lib/math.js
+++ src/lib/math.js
@@ -1,19 +1,93 @@
+/**
+ * sum is used to add two numbers together, and return the result. If
+ * either of the paramters are undefined, it returns null
+ *
+ * @param a - the first number for the expression
+ * @param b - the second number for the expression
+ */
 export function sum (a, b) {
-  return;
+  let output = null;
+
+  if (a != undefined && b != undefined) {
+    output = a + b;
+  }
+  return output;
 }
 
+/**
+ * subtract is used to ubtract one number from another, and return
+ * the result. If either of the paramters are undefined, it returns
+ * null
+ *
+ * @param a - the first number for the expression
+ * @param b - the second number for the expression
+ */
 export function subtract(a, b) {
-  return;
+  let output = null;
+
+  if (a != undefined && b != undefined) {
+    output = a - b;
+  }
+  return output;
 }
 
+/**
+ * divide is used to divide on number by another, and return the
+ * result. If either of the paramters are undefined, it returns
+ * null. If the divisor is zero, we also return null.
+ *
+ * @param a - the first number for the expression
+ * @param b - the second number for the expression
+ */
 export function divide (a, b) {
-  return;
+  let output = null;
+
+  if (a != undefined && b != undefined && b != 0) {
+    output = a / b;
+  }
+  return output;
 }
 
+/**
+ * * is used to multiply two numbers together, and return the
+ * result. If either of the paramters are undefined, it
+ * returns null
+ *
+ * @param a - the first number for the expression
+ * @param b - the second number for the expression
+ */
 export function multiply (a, b) {
-  return;
+  let output = null;
+
+  if (a != undefined && b != undefined) {
+    output = a * b;
+  }
+  return output;
 }
 
+/**
+ * sum is used to calculate the mean of an array of numbers,
+ * and return the result. If any of the elements of the array
+ * are null, we return null
+ *
+ * @param a - the array of numbers to get the mean of
+ */
 export function mean (a) {
-  return;
+  let output = null;
+  let total = 0;
+  let hasNull = false;
+
+  if (a != undefined && a.length != 0) {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] == null) {
+        hasNull = true;
+        break;
+      }
+      total += a[i];
+    }
+    if (!hasNull) {
+      output = total / a.length;
+    }
+  }
+  return output;
 }
